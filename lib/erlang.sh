221b982f47b7cf531470fdb7c9d58c36833292a8
Tyler Flint <tylerflint@gmail.com>
2016-10-07 09:05:43 -0600

    Start over following established pattern

--- lib/erlang.sh
+++ lib/erlang.sh
@@ -1,19 +1,39 @@
 # -*- mode: bash; tab-width: 2; -*-
 # vim: ts=2 sw=2 ft=bash noet
 
-runtime() {
-	echo $(nos_validate "$(nos_payload 'config_runtime')" "string" "erlang-18")
+# Copy the code into the live directory which will be used to run the app
+publish_release() {
+  nos_print_bullet "Moving build into live app directory..."
+  rsync -a $(nos_code_dir)/ $(nos_app_dir)
 }
 
-install_runtime() {
-  pkgs=($(runtime))
+# Determine the erlang runtime to install. This will first check
+# within the boxfile.yml, then will rely on default_runtime to
+# provide a sensible default
+runtime() {
+  echo $(nos_validate \
+    "$(nos_payload "config_runtime")" \
+    "string" "$(default_runtime)")
+}
 
-	nos_install ${pkgs[@]}
+# Provide a default erlang version.
+default_runtime() {
+  echo "erlang-18"
+}
+
+# Install the erlang runtime along with any dependencies.
+install_runtime_packages() {
+  pkgs=("$(runtime)")
+  
+  # add any client dependencies
+  pkgs+=("$(query_dependencies)")
+
+  nos_install ${pkgs[@]}
 }
 
 # Uninstall build dependencies
 uninstall_build_packages() {
-  # currently ruby doesn't install any build-only deps... I think
+  # currently erlang doesn't install any build-only deps... I think
   pkgs=()
 
   # if pkgs isn't empty, let's uninstall what we don't need
@@ -22,27 +42,14 @@ uninstall_build_packages() {
   fi
 }
 
-make_target() {
-	echo $(nos_validate "$(nos_payload 'config_make_target')" "string" "")
-}
+# compiles a list of dependencies that will need to be installed
+query_dependencies() {
+  deps=()
 
-compile() {
-	if [[ -f $(nos_code_dir)/Makefile ]]; then
-		(cd $(nos_code_dir); nos_run_process "make $(make_target)" "make $(make_target)")
-	fi
-}
-
-project_dir() {
-	echo $(nos_validate "$(nos_payload 'config_project_dir')" "string" "")
-}
-
-# Copy the compiled jars into the app directory to run live
-publish_release() {
-	nos_print_bullet "Moving build into live code directory..."
-	if [[ ! -z "$(project_dir)" ]]; then
-		mkdir -p $(nos_app_dir)/$(project_dir)
-		rsync -a $(nos_code_dir)/ $(nos_app_dir)/$(project_dir)
-	else
-		rsync -a $(nos_code_dir)/ $(nos_app_dir)
-	fi
+  # mysql
+  # memcache
+  # postgres
+  # redis
+  
+  echo "${deps[@]}"
 }
